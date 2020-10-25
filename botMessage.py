30347776df3bca47ccb43ae3b5c6f0a3d89c7bf9
EC2 Default User <ec2-user@ip-172-31-39-121.ec2.internal>
2020-10-25 21:29:52 +0000

    5 tasks that that checked unmocked_unit tests

--- botMessage.py
+++ botMessage.py
@@ -9,16 +9,16 @@ def validMessage(message):
             KEY_BOT_COMMAND: None,
             KEY_MESSAGE: message
     }
-    message = message[:2]
+    message = message[2:]
     message = message.strip()
     message_components = message.split(" ",1)
     if len(message_components) == 1:
         possible_bot_cmd, rest_of_message = message_components[0],""
     else:
         possible_bot_cmd, rest_of_message = message_components[0], message_components[1]
+        
     return{
         KEY_IS_BOT: True,
-        KEY_BOT_COMMAND: possible_bot_cmd[0],
+        KEY_BOT_COMMAND: possible_bot_cmd,
         KEY_MESSAGE: rest_of_message
     }
-    
