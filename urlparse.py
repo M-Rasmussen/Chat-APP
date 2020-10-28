f722f3a7a2f4d827e7acd847596fab4035586cc3
EC2 Default User <ec2-user@ip-172-31-39-121.ec2.internal>
2020-10-28 02:55:18 +0000

    used black on urlparse.py

--- urlparse.py
+++ urlparse.py
@@ -1,27 +1,28 @@
+'''parse the file to see if there is any url or picture in it'''
 from rfc3987 import parse
 
-    
+
 def url_parse(usermessage):
-    messageList=usermessage.split()
-    picimages=["jpg","png","gif"]
-    rtnMessage=''
+    '''pars the message to see if any url or pic is in it'''
+    messageList = usermessage.split()
+    picimages = ["jpg", "png", "gif"]
+    rtnMessage = ""
     for message in messageList:
         try:
-            parsedinfo=parse(message,rule='IRI')
-            print(parsedinfo.get('scheme'))
-            urlpic=parsedinfo.get('path')
-            if (any(pic in urlpic for pic in picimages)):
-                picRtn='<imgsrc='
-                picRtn+=message
-                rtnMessage+=picRtn
-                rtnMessage+=" "
+            parsedinfo = parse(message, rule="IRI")
+            print(parsedinfo.get("scheme"))
+            urlpic = parsedinfo.get("path")
+            if any(pic in urlpic for pic in picimages):
+                picRtn = "<imgsrc="
+                picRtn += message
+                rtnMessage += picRtn
+                rtnMessage += " "
             else:
-                atag='<ahref='
-                atag+=message
-                rtnMessage+=atag
-                rtnMessage+=" "
+                atag = "<ahref="
+                atag += message
+                rtnMessage += atag
+                rtnMessage += " "
         except:
-            rtnMessage+=message
-            rtnMessage+= " "
+            rtnMessage += message
+            rtnMessage += " "
     return rtnMessage
-            
